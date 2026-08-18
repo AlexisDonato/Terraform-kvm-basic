dc334ad69bf63832a1a7719628c9bfe08e524756
AlexisDonato <alek6.don@email.com>
2026-08-18 08:57:50 +0200

    Upd : creating Debian libvirt volume and adding comments

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+# Terraform configuration
+# Define the libvirt provider used to manage our KVM infrastructure.
 terraform {
   required_providers {
     libvirt = {
@@ -7,20 +9,25 @@ terraform {
   }
 }
 
+# Connect Terraform to the system libvirt instance.
 provider "libvirt" {
   uri = "qemu:///system"
 }
 
+# Create a libvirt volume from an existing Debian cloud image.
+# The volume will be stored in libvirt's "default" storage pool.
 resource "libvirt_volume" "debian_disk" {
   name = "terraform-basic-disk.qcow2"
   pool = "default"
 
+  # Define the disk format.
   target = {
     format = {
       type = "qcow2"
     }
   }
 
+  # Use the existing Debian image as the source for the new volume.
   create = {
     content = {
       url = "/root/devops-lab/images/debian-13-generic-amd64.qcow2"
