94c64093829563bcf630e28f716ea0279bfde5ea
AlexisDonato <alek6.don@email.com>
2026-08-18 08:46:59 +0200

    Fix : correcting main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     libvirt = {
-      source = "dmacvicar/libvirt"
+      source  = "dmacvicar/libvirt"
       version = "0.9.8"
     }
   }
@@ -9,4 +9,21 @@ terraform {
 
 provider "libvirt" {
   uri = "qemu:///system"
+}
+
+resource "libvirt_volume" "debian_disk" {
+  name = "terraform-basic-disk.qcow2"
+  pool = "default"
+
+  target = {
+    format = {
+      type = "qcow2"
+    }
+  }
+
+  create = {
+    content = {
+      url = "/root/devops-lab/images/debian-13-generic-amd64.qcow2"
+    }
+  }
 }
